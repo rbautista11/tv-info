6f685ef12bc4b3119bfa86dd17acedd125489079
rafael <323bautista@gmail.com>
2016-08-29 17:34:30 -0700

    Can now retrieve poster and inject into html

--- popup.js
+++ popup.js
@@ -3,6 +3,10 @@
 chrome.tabs.executeScript( {
     code: "window.getSelection().toString();"
 }, function(selection) {
-    var id = findID(selection[0]);
-    
+    findID(selection[0], function(id) {
+      console.log(id);
+      getPoster(id, function(posterSource) {
+        document.getElementById("posterImage").src = posterSource;
+      });
+    });
 });
--- trakttv.js
+++ trakttv.js
@@ -1,5 +1,6 @@
 
-function findID(input) {
+
+function findID(input, callback) {
   var request = new XMLHttpRequest();
   var query = 'https://api.trakt.tv/search/show?query=' + input;
 
@@ -15,32 +16,54 @@ function findID(input) {
       console.log('Headers:', this.getAllResponseHeaders());
       console.log('Body:', this.responseText);
       var obj = JSON.parse(this.responseText);
-      return obj[0].show.ids.slug);
+      callback(obj[0].show.ids.slug);
     }
   };
 
   request.send();
 }
 
+function getPoster(id, callback) {
+
+  var request = new XMLHttpRequest();
+  var query = 'https://api.trakt.tv/shows/' + id + '?extended=images';
+
+  request.open('GET', query);
+
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.setRequestHeader('trakt-api-version', '2');
+  request.setRequestHeader('trakt-api-key', apiKey);
+
+  request.onreadystatechange = function () {
+    if(this.readyState == 4) {
+      console.log('Status:', this.status);
+      console.log('Headers:', this.getAllResponseHeaders());
+      console.log('Body:', this.responseText);
+      var obj = JSON.parse(this.responseText);
+      callback(obj.images.poster.thumb);
+
+    }
+  };
+
+  request.send();
+}
+
+function getSummary(id) {
+  var request = new XMLHttpRequest();
+
+  request.open('GET', 'https://api.trakt.tv/shows/game-of-thrones');
+
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.setRequestHeader('trakt-api-version', '2');
+  request.setRequestHeader('trakt-api-key', apiKey);
 
 
-
-/*
-var request = new XMLHttpRequest();
-
-request.open('GET', 'https://api.trakt.tv/shows/game-of-thrones');
-
-request.setRequestHeader('Content-Type', 'application/json');
-request.setRequestHeader('trakt-api-version', '2');
-request.setRequestHeader('trakt-api-key', apiKey);
-
-
-request.onreadystatechange = function () {
-  if(this.readyState == 4) {
-    console.log('Status:', this.status);
-    console.log('Headers:', this.getAllResponseHeaders());
-    console.log('Body:', this.responseText);
-  }
-};
-request.send();
-*/
+  request.onreadystatechange = function () {
+    if(this.readyState == 4) {
+      console.log('Status:', this.status);
+      console.log('Headers:', this.getAllResponseHeaders());
+      console.log('Body:', this.responseText);
+    }
+  };
+  request.send();
+}
