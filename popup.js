75442993b84ff55d6b42384cd09c4fb6211d1a64
rafael <323bautista@gmail.com>
2016-08-30 12:51:03 -0700

    Added some styling

--- popup.js
+++ popup.js
@@ -3,16 +3,19 @@
 chrome.tabs.executeScript( {
     code: "window.getSelection().toString();"
 }, function(selection) {
+    if(!selection[0] || selection[0].length === 0){
+      return;
+    }
     findID(selection[0], function(id) {
       console.log(id);
       getPoster(id, function(posterSource) {
         document.getElementById("posterImage").src = posterSource;
         getSummary(id, function(title, rating, runtime, network, year, overview) {
           document.getElementById("title").innerHTML = title;
-          document.getElementById("rating").innerHTML = rating;
-          document.getElementById("runtime").innerHTML = runtime;
-          document.getElementById("network").innerHTML = network;
-          document.getElementById("year").innerHTML = year;
+          document.getElementById("rating").innerHTML = "Rating: " + rating;
+          document.getElementById("runtime").innerHTML = "Runtime: " + runtime + " min";
+          document.getElementById("network").innerHTML = "Network: " + network;
+          document.getElementById("year").innerHTML = "Year: " + year;
           document.getElementById("overview").innerHTML = overview;
 
         });
